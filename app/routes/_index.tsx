1146f610f08b444be94c76574622c96eba0b5296
Brandon Lorenz <blorenz@gmail.com>
2024-04-16 16:39:17 -0400

    first

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,41 +1,67 @@
 import type { MetaFunction } from "@remix-run/node";
-
+import { AnimatePresence, motion } from "framer-motion";
+import { useState } from "react";
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: "New Remix App" },
+		{ name: "description", content: "Welcome to Remix!" },
+	];
 };
 
 export default function Index() {
-  return (
-    <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1>Welcome to Remix</h1>
-      <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
-      </ul>
-    </div>
-  );
+	const [values, setValues] = useState<string[]>([]);
+
+	return (
+		<div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
+			<motion.div
+				initial={{
+					opacity: 0,
+				}}
+				animate={{ opacity: 1 }}
+				exit={{ opacity: 0 }}
+				transition={{ duration: 5 }}
+			>
+				Testing
+			</motion.div>
+			<div>
+				<motion.div
+					layout
+					initial={{ opacity: 0 }}
+					transition={{ duration: 3 }}
+					animate={{ opacity: 1 }}
+					exit={{ opacity: 0 }}
+					className={`bg-blue-500`}
+				>
+					Testing!
+				</motion.div>
+				<div>
+					<AnimatePresence initial={false}>
+						{values.map((m) => (
+							<motion.div
+								layout
+								initial={{ opacity: 0 }}
+								transition={{ duration: 0.2 }}
+								animate={{ opacity: 1 }}
+								exit={{ opacity: 0 }}
+								key={m}
+							>
+								{m}
+							</motion.div>
+						))}
+					</AnimatePresence>
+
+					<button
+						type={"button"}
+						onClick={(e) => {
+							setValues((vals) => {
+								return [...vals, Math.random()];
+							});
+						}}
+					>
+						click
+					</button>
+				</div>
+			</div>
+		</div>
+	);
 }
